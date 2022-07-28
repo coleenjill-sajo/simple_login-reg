e58965e559116a664e931d66f271c40e8f261a79
unknown <coleenjill.sajo@g.batstate-u.edu.ph>
2022-07-28 16:21:02 +0800

    Configured Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDH5cwmDv0oPVav6HYUvcKF2p0qCktPpAc",
+    authDomain: "simple-loginreg.firebaseapp.com",
+    projectId: "simple-loginreg",
+    storageBucket: "simple-loginreg.appspot.com",
+    messagingSenderId: "519913639931",
+    appId: "1:519913639931:web:366cb3153b74b959ff455e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDH5cwmDv0oPVav6HYUvcKF2p0qCktPpAc",
+    authDomain: "simple-loginreg.firebaseapp.com",
+    projectId: "simple-loginreg",
+    storageBucket: "simple-loginreg.appspot.com",
+    messagingSenderId: "519913639931",
+    appId: "1:519913639931:web:366cb3153b74b959ff455e"
+  }
 };
 
 /*
