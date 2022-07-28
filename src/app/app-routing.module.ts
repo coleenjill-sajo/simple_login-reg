0cb097b4209f590601ff8135d97361574af2d7e6
unknown <coleenjill.sajo@g.batstate-u.edu.ph>
2022-07-28 11:53:08 +0800

    Added 'main-page and Imported HTTPClient, HTTPClientModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },  {
+    path: 'main-page',
+    loadChildren: () => import('./main-page/main-page.module').then( m => m.MainPagePageModule)
   },
+
 ];
 
 @NgModule({
---
+++ src/app/main-page/main-page.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.page.html',
+  styleUrls: ['./main-page.page.scss'],
+})
+export class MainPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
